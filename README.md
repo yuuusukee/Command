e8158f7fb9f567b06890584076d20a6624dfb328
yuuusukee <ynakakoji731@gmail.com>
2022-06-13 21:57:00 +0900

    changed error message

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Impa
+
+Stores the file names of huge amounts of data, such as images, in CSV data. Columns can be specified.
--- impa.c
+++ impa.c
@@ -1,21 +1,41 @@
 #include <stdio.h>
 #include <dirent.h>
 #include <stdlib.h>
+#include<string.h>
 
+static void dir_error(){
+	printf("Directory not found");
+	exit(0);
+}
+static void argument_error(){
+	printf("Please specify 3 arguments\n");
+	exit(0);
+}
 int main(int argc, char* argv[]){
 	DIR *dir;
 	FILE *fp;
+	if(argc != 4){
+		argument_error();
+	}
+	char* ref_der = argv[1];
+	char* gen_file = argv[2];
+	char* column_name = argv[3];
+	fp = fopen(gen_file,"w");
 
-	fp = fopen(argv[2],"w");
-	fprintf(fp,argv[3]);
+	fprintf(fp,column_name);
 	fprintf(fp,"\n");
 	struct dirent *ds;
-	dir=opendir(argv[1]);
-	for(ds = readdir(dir); ds != NULL; ds = readdir(dir) ){
-		fprintf(fp,ds->d_name);
-		fprintf(fp,"\n");
+	dir=opendir(ref_der);
+	if(dir == 0){
+		dir_error();
 	}
-
+	for(ds = readdir(dir); ds != NULL; ds = readdir(dir) ){
+		printf("%s",ds[0]);
+		if(strcmp(ds->d_name[0],".")==1 || strcmp(ds->d_name,"..")==1){
+			fprintf(fp,ds->d_name);
+			fprintf(fp,"\n");
+		}
+	}
 	closedir(dir);
 	fclose(fp);
 	return 0;
